db320de2148ebf234c6eb38b6ae08bc5b3f931e1
Сергей <s.v.yakovlev@yandex.ru>
2022-09-21 23:13:23 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> stringList =new ArrayList<>();
+    }
+}
